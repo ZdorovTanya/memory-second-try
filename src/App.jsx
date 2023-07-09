8060210326817d850a8d8ac63c46b972b11a7620
ZdorovTanya <zdorovilo.tanya@mail.ru>
2023-07-09 13:38:40 +0300

    project structure

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const items = [
+  //   { emoji: "🐏", id: "1", matchFound: false, flipped:false },
+  //   { emoji: "🐑", id: "2", matchFound: false, flipped:false },
+  //   { emoji: "🐪", id: "3", matchFound: false, flipped:false },
+  //   { emoji: "🐫", id: "4", matchFound: false, flipped:false },
+  //   { emoji: "🦕", id: "5", matchFound: false, flipped:false },
+  //   { emoji: "🦖", id: "6", matchFound: false, flipped:false },
+  //   { emoji: "🐳", id: "7", matchFound: false, flipped:false },
+  //   { emoji: "🐋", id: "8", matchFound: false, flipped:false },
+  // ];
+  return (
+    <>
+     <h1>Memory game</h1>
+     <button>New game</button>
+    </>
+  )
+}
+
+export default App
